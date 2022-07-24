bf5b3461892df43c63fc2135e0cd77db31a2fd06
Fabrício Girardi Andreis <fabricio.andreis@gmail.com>
2022-07-24 15:42:48 -0300

    wip: builder pattern

---
+++ patterns/creational/builder.go
@@ -0,0 +1,5 @@
+package creational
+
+// Builder is a creational design pattern that lets you construct complex objects step by step.
+// The pattern allows you to produce different types and representations of an object using the same construction code.
+// https://refactoring.guru/design-patterns/builder
