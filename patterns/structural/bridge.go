c8c68ffe283cecd44eea399a37097bf356f3dba4
Fabrício Girardi Andreis <fabricio.andreis@gmail.com>
2022-08-13 08:04:33 -0300

    feat: finish implementation of a bridge design pattern

--- patterns/structural/bridge.go
+++ patterns/structural/bridge.go
@@ -1,5 +1,7 @@
 package structural
 
+import "fmt"
+
 // Bridge is a structural design pattern that lets you split a large class or a set
 // of closely related classes into two separate hierarchies—abstraction and implementation—which
 // can be developed independently of each other.
@@ -7,13 +9,42 @@ package structural
 
 // A mechanism that decouples an interface (hierarchy) from an implementation (hierarchy)
 
+// Renderer is the Implementation interface
 type Renderer interface {
 	RenderCircle(radius float64)
 }
 
+// VectorRenderer is a Concrete Implementation
 type VectorRenderer struct {
 }
 
 func (v *VectorRenderer) RenderCircle(radius float64) {
+	fmt.Println("Drawing a circle of radius", radius)
+}
 
+// RasterRenderer is a Concrete Implementation
+type RasterRenderer struct {
+	Dpi int
+}
+
+func (r *RasterRenderer) RenderCircle(radius float64) {
+	fmt.Println("Drawing pixels for a circle of radius", radius)
+}
+
+// Circle is the Abstraction
+type Circle struct {
+	renderer Renderer
+	radius   float64
+}
+
+func NewCircle(renderer Renderer, radius float64) *Circle {
+	return &Circle{renderer: renderer, radius: radius}
+}
+
+func (c *Circle) Draw() {
+	c.renderer.RenderCircle(c.radius)
+}
+
+func (c *Circle) Resize(factor float64) {
+	c.radius *= factor
 }
--- patterns/structural/bridge_test.go
+++ patterns/structural/bridge_test.go
@@ -1,1 +1,34 @@
 package structural_test
+
+import (
+	"testing"
+
+	"github.com/fabricioandreis/design-patterns-go/patterns/structural"
+)
+
+func TestBridge(t *testing.T) {
+
+	t.Run("Should draw a circle with vector renderer", func(t *testing.T) {
+		v := structural.VectorRenderer{}
+		c := structural.NewCircle(&v, 100)
+
+		c.Draw()
+	})
+
+	t.Run("Should draw a circle with raster renderer", func(t *testing.T) {
+		v := structural.RasterRenderer{}
+		c := structural.NewCircle(&v, 100)
+
+		c.Draw()
+	})
+
+	t.Run("Should draw a resized circle with raster renderer", func(t *testing.T) {
+		v := structural.RasterRenderer{}
+		c := structural.NewCircle(&v, 100)
+
+		c.Draw()
+		c.Resize(200)
+		c.Draw()
+	})
+
+}
