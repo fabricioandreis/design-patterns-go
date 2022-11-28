e32662f978c64861f4117b7cfb260727b4778228
Fabrício Girardi Andreis <fabricio.andreis@gmail.com>
2022-11-28 19:57:12 -0300

    feat: State behavioral design pattern

---
+++ patterns/behavioral/state_test.go
@@ -0,0 +1,16 @@
+package behavioral_test
+
+import (
+	"testing"
+
+	"github.com/fabricioandreis/design-patterns-go/patterns/behavioral"
+)
+
+func TestState(t *testing.T) {
+	t.Run("Should be able to transition a state", func(t *testing.T) {
+		sw := behavioral.NewSwitch()
+		sw.On()
+		sw.Off()
+		sw.Off()
+	})
+}
